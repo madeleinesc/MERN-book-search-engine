fa15c0ab88bc8e1a6420d31232044f22d56adc82
Madeleinesc <madeleine.s.cosway@gmail.com>
2022-02-06 23:13:10 +1030

    added imports to server.js, created schema folder

--- server/server.js
+++ server/server.js
@@ -3,8 +3,24 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+//imports:
+//apollo server
+//authMiddleware
+//schemas
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+
+// Update Express.js to use Apollo server features
+server.applyMiddleware({ app });
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +30,14 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    // log where we can go to test our GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
