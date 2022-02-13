f5a430324cdd97a3dfc369cf295b86b3fc24eb26
Madeleinesc <madeleine.s.cosway@gmail.com>
2022-02-13 17:10:38 +1030

    added ApolloProvider to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -16,6 +17,7 @@ function App() {
         </Switch>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
